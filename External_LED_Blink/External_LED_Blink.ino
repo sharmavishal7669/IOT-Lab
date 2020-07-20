47d5cd5d20b2679ee4b35e59877553096287f7ae
Vishal Sharma <vsunite3@gmail.com>
2020-07-20 19:37:15 +0530

    Added Parallel led blinking program

--- External_LED_Blink/External_LED_Blink.ino
+++ External_LED_Blink/External_LED_Blink.ino
@@ -1,13 +1,19 @@
+#define pin 1               // using macro tp fix value of pin
+
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(1, OUTPUT);
+  pinMode(pin, OUTPUT);
+  pinMode(2, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(1, HIGH);   // turn the LED on 
+  digitalWrite(pin, HIGH);   // turn the LED on
+  digitalWrite(2, HIGH);
   delay(1000);                       // wait for a second
-  digitalWrite(1, LOW);    // turn the LED off
+  digitalWrite(pin, LOW);    // turn the LED off
+  digitalWrite(2, LOW);
   delay(1000);                       // wait for a second
 }
